17bc49be2ea70014d8e7a42b1395ccc5889b0b45
Abhi Yerra <abhi.yerra@lookout.com>
2014-12-10 11:30:45 -0800

    Simplify and upload files to the appropriate location.

--- sarpa.go
+++ sarpa.go
@@ -21,6 +21,7 @@ var (
 type Config struct {
 	etcdClient *etcd.Client `json:"-"`
 	awsAuth    aws.Auth     `json:"-"`
+	s3Auth     *s3.S3       `json:"-"`
 
 	EtcdHosts []string            `json:"etcd_hosts"`
 	S3Bucket  string              `json:"-"`
@@ -32,9 +33,9 @@ func (c *Config) GetServices() {
 
 	// Get the nodes and their values.
 	resp, err := c.etcdClient.Get(EtcdSarpaPath, true, true)
-
 	if err != nil {
 		log.Println(err)
+		c.Services = newServices
 		return
 	}
 
@@ -61,56 +62,55 @@ func (c *Config) GetServices() {
 func (c *Config) jsonedServices() []byte {
 	data, err := json.Marshal(c.Services)
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
-
-	log.Println(data)
 
 	return data
 }
 
 func (c *Config) UploadToS3() {
-	s := s3.New(c.awsAuth, aws.APNortheast)
-	bucket := s.Bucket(c.S3Bucket)
+	if c.s3Auth == nil {
+		c.s3Auth = s3.New(c.awsAuth, aws.USEast)
+	}
+	bucket := c.s3Auth.Bucket(c.S3Bucket)
 
-	err := bucket.Put("/discovery.json", c.jsonedServices(), "application/json", s3.PublicRead, s3.Options{})
+	err := bucket.Put("discovery.json", c.jsonedServices(), "text/javascript", s3.PublicRead, s3.Options{})
 	if err != nil {
-		panic(err.Error())
+		log.Println(err)
 	}
 }
 
 func (c *Config) EtcdConnect(etcdHosts []string) {
 	c.EtcdHosts = etcdHosts
 	c.etcdClient = etcd.NewClient(c.EtcdHosts)
-
 }
 
 func (c *Config) AwsConnect() {
 	auth, err := aws.EnvAuth()
 	if err != nil {
-		panic(err.Error())
+		log.Println(err)
 	}
 
 	c.awsAuth = auth
 }
 
-func (c *Config) StartWatchmen(upload chan bool) {
+func (c *Config) StartWatchmen() {
 	log.Println("Starting watchman.")
 
-	// Need to run it the first time to reset the stale config.
-	c.GetServices()
-
 	for {
+		log.Println("Waiting for an update...")
+
 		// Watch for changes to the values
 		watchChan := make(chan *etcd.Response)
 		go c.etcdClient.Watch(EtcdSarpaPath, 0, true, watchChan, nil)
-		log.Println("Waiting for an update...")
 
 		select {
 		case r := <-watchChan:
 			log.Printf("updated keys: %s: %s\n", r.Node.Key, r.Node.Value)
 			c.GetServices()
-			upload <- true
+
+			log.Println("Uploading to S3.")
+			c.UploadToS3()
 		}
 	}
 }
@@ -121,20 +121,17 @@ func init() {
 		etcdHost = "http://127.0.0.1:4001"
 	}
 
-	config := &Config{}
+	config = &Config{}
 	config.EtcdConnect([]string{etcdHost})
 	config.AwsConnect()
+
+	config.S3Bucket = os.Getenv("SARPA_BUCKET")
 }
 
 func main() {
-	upload := make(chan bool)
+	// Need to run it the first time to reset the stale config.
+	config.GetServices()
+	config.UploadToS3()
 
-	config.StartWatchmen(upload)
-
-	for {
-		<-upload
-
-		log.Println("Uploading to S3.")
-		config.UploadToS3()
-	}
+	config.StartWatchmen()
 }
